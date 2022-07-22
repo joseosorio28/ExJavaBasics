8ab59bf70c39ec0bd79697b65193107ad5bc3e4e
jose.osorio <jose.osorio@pragma.com.co>
2022-07-22 10:31:08 -0500

    Refactor, added classes

---
+++ src/test/main/ExercisesTest.java
@@ -0,0 +1,34 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+class ExercisesTest {
+
+    private Exercises obj = new Exercises();
+    private Exercises.Calculator calculator = new Exercises().new Calculator();
+
+    @Test
+    void canFindMinAndMaxNumbers() {
+    }
+
+    @Test
+    void canCreateRandomVector() {
+    }
+
+    @Test
+    void canCalculatorSum() {
+    }
+
+    @Test
+    void canCalculatorSubstract() {
+    }
+
+    @Test
+    void canCalculatorMultiply() {
+    }
+
+    @Test
+    void canCalculatorDivide() {
+
+    }
+}
